b71060e3f17087c85b62ab477301c6ed93f0af45
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-01 14:20:01 -0700

    fixed functionality a broke when switching to thiss

--- Shopping-List/Shopping-List.js
+++ Shopping-List/Shopping-List.js
@@ -4,7 +4,7 @@
 function addItems() {
   $('#js-shopping-list-form').submit(function (event) {
     event.preventDefault();
-    const userText = this.find('.js-shopping-list-entry');
+    const userText = $(this).find('.js-shopping-list-entry');
     $('ul').append(`
         <li>
         <span class="shopping-item">${userText.val()}</span>
@@ -24,13 +24,13 @@ function addItems() {
 
 function checkAndUncheckItems (){
   $('ul').on('click', '.shopping-item-toggle', function(event){
-    this.parent().siblings('.shopping-item').toggleClass('shopping-item__checked');
+    $(this).parent().siblings('.shopping-item').toggleClass('shopping-item__checked');
   });
 }
 
 function deleteItems(){
   $('ul').on('click', '.shopping-item-delete', function(event) {
-    this.closest('li').remove();
+    $(this).closest('li').remove();
   });
 }
 
